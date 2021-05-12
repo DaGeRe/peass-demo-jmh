b8dc3eeebd9defb03fe874e9bbf64536b39925e9
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-12 15:13:48 +0200

    Added demo-project-jmh

--- executeAll.sh
+++ executeAll.sh
@@ -6,8 +6,8 @@ git clone --branch jmh https://github.com/DaGeRe/peass.git && \
 	./mvnw clean install -DskipTests=true -V
 
 # If minor updates to the project occur, the version name may change
-VERSION="40b6aa16322efee0ec6c9ddc835e281cc265f88b"
-PREVIOUS_VERSION="50101b91bd5b3a43cb27c171829872ebad957ad7"
+VERSION="90c2c44be016087603b2e29e49fd4e4d2115d0ee"
+PREVIOUS_VERSION="90d82a78c2d7c15785fe54ea496fa83db3c8f873"
 
 DEMO_PROJECT_JMH_PEASS=../demo-project-jmh_peass
 EXECUTE_FILE=results/execute_demo-project-jmh.json
