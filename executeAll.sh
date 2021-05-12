e0207aff58364a55fe6c2b163d997376e8e23aa8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-12 18:10:40 +0200

    Add change reading

--- executeAll.sh
+++ executeAll.sh
@@ -7,9 +7,8 @@ git clone --branch jmh https://github.com/DaGeRe/peass.git && \
 
 # If minor updates to the project occur, the version name may change
 VERSION="90c2c44be016087603b2e29e49fd4e4d2115d0ee"
-PREVIOUS_VERSION="90d82a78c2d7c15785fe54ea496fa83db3c8f873"
 
-DEMO_PROJECT_JMH_PEASS=../demo-project-jmh_peass
+DEMO_PROJECT_PEASS=../demo-project-jmh_peass
 EXECUTE_FILE=results/execute_demo-project-jmh.json
 
 # It is assumed that $DEMO_HOME is set correctly and PeASS has been built!
@@ -18,3 +17,16 @@ echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
 
 echo ":::::::::::::::::::::MEASURE::::::::::::::::::::::::::::::::::::::::::"
 ./peass measure -executionfile $EXECUTE_FILE -folder $DEMO_HOME -workloadType JMH -iterations 1 -warmup 0 -repetitions 1 -vms 2
+
+./peass getchanges -data ../demo-project-jmh_peass/ -dependencyfile results/deps_demo-project-jmh.json
+test_sha=$(grep -A1 'versionChanges" : {' results/changes_demo-project-jmh.json | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
+if [ "$VERSION" != "$test_sha" ]
+then
+    echo "commit-SHA ("$VERSION") is not equal to the SHA in changes_demo-project.json ("$test_sha")!"
+    cat results/statistics/demo-project-jmh.json
+    exit 1
+else
+    echo "changes_demo-project.json contains the correct commit-SHA."
+fi
+
+
