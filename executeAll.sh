172cd0f3aea1be6f358e07446d117f26b3f7cac2
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-17 09:52:25 +0200

    Separate setting of DEMO_HOME

--- executeAll.sh
+++ executeAll.sh
@@ -2,13 +2,13 @@
 tar -xf demo-project-jmh.tar.xz
 git clone --branch jmh https://github.com/DaGeRe/peass.git && \
 	cd peass && \
-	DEMO_HOME=$(pwd)/../demo-project-jmh && \
 	./mvnw clean install -DskipTests=true -V
 
 # If minor updates to the project occur, the version name may change
 VERSION="90c2c44be016087603b2e29e49fd4e4d2115d0ee"
 INITIALVERSION="90d82a78c2d7c15785fe54ea496fa83db3c8f873"
 
+DEMO_HOME=$(pwd)/../demo-project-jmh
 DEMO_PROJECT_PEASS=../demo-project-jmh_peass
 EXECUTE_FILE=results/execute_demo-project-jmh.json
 
