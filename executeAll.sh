02f162d9b428dad1ac55b8d6a17c1b7da11101b9
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-17 10:42:16 +0200

    Add checking of selection results

--- executeAll.sh
+++ executeAll.sh
@@ -7,6 +7,7 @@ git clone --branch jmh https://github.com/DaGeRe/peass.git && \
 
 # If minor updates to the project occur, the version name may change
 VERSION="90c2c44be016087603b2e29e49fd4e4d2115d0ee"
+INITIALVERSION="90d82a78c2d7c15785fe54ea496fa83db3c8f873"
 
 DEMO_PROJECT_PEASS=../demo-project-jmh_peass
 EXECUTE_FILE=results/execute_demo-project-jmh.json
@@ -14,6 +15,13 @@ EXECUTE_FILE=results/execute_demo-project-jmh.json
 # It is assumed that $DEMO_HOME is set correctly and PeASS has been built!
 echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
 ./peass select -folder $DEMO_HOME -workloadType JMH
+
+initialSelected=$(grep "initialversion" -A 1 results/deps_demo-project-jmh.json | grep "\"version\"" | tr -d " \"," | awk -F':' '{print $2}') 
+if [ "$initialSelected" != "$INITIALVERSION" ]
+then
+	echo "Initialversion should be $INITIALVERSION, but was $initialSelected"
+	exit 1
+fi
 
 echo ":::::::::::::::::::::MEASURE::::::::::::::::::::::::::::::::::::::::::"
 ./peass measure -executionfile $EXECUTE_FILE -folder $DEMO_HOME -workloadType JMH -iterations 1 -warmup 0 -repetitions 1 -vms 2
