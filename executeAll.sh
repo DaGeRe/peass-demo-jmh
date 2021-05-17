de602892fea6c1e418696fbceb9181482935a069
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-05-17 09:54:03 +0200

    Updated SHA.

--- executeAll.sh
+++ executeAll.sh
@@ -5,7 +5,7 @@ git clone --branch jmh https://github.com/DaGeRe/peass.git && \
 	./mvnw clean install -DskipTests=true -V
 
 # If minor updates to the project occur, the version name may change
-VERSION="90c2c44be016087603b2e29e49fd4e4d2115d0ee"
+VERSION="1e4373dd2e642ff598e567baa0f3e2325eb635de"
 INITIALVERSION="90d82a78c2d7c15785fe54ea496fa83db3c8f873"
 
 DEMO_HOME=$(pwd)/../demo-project-jmh
